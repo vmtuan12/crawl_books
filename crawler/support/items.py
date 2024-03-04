db4edb0a1d44c8b2e682a4d5563573dae19f9056
vmtuan12 <minhtuan3154@gmail.com>
2024-03-04 18:00:34 +0700

    feat: add baseitem class

--- crawler/support/items.py
+++ crawler/support/items.py
@@ -1,4 +1,12 @@
-class BookItem:
+class BaseItem:
+    def to_json(self) -> dict:
+        result = {}
+        for attr in self.__dict__:
+            result.update({attr: self.__dict__[attr]})
+
+        return result
+
+class BookItem(BaseItem):
     def __init__(self, name: str, 
                  description: str, 
                  genres: list[str], 
