902ba2c36a93ed7a8cf66e34160ca49064a0f272
vmtuan12 <minhtuan3154@gmail.com>
2024-03-15 20:42:02 +0700

    feat[mtuan]: read and save data from csv

--- dataset_handle/read_data.py
+++ dataset_handle/read_data.py
@@ -1,31 +1,44 @@
 import pandas as pd
 import re
 import ast
+import os
+from datetime import datetime
 from pipeline import pipeline, topic
+import json
 
 class BookCsvReader:
+    special_chars_set = {'+', '-', '=', '&', '|', '|', '>', '<', '!', '(', ')', '{', '}', '[', ']', '^', '"', '~', '*', '?', ':', '\\', '/', ',', '.'}
     list_regex = r"^[[].*[]]$"
+    date_regex = r"(\d{1,2})\/(\d{1,2})\/(\d{4})"
+    year_regex = r"\d{4}"
+    number_regex = r"\d+(,\d+)*"
     kafka_connector = pipeline.KafkaConnector()
 
     def read_and_save_data(self, path_to_files='dataset_handle/raw_data'):
-        # for file in os.listdir(path_to_files):
-        #     file_name, file_extension = os.path.splitext(file)
-        data = pd.read_csv(path_to_files + "/" + "dataset0_0.csv")
-        data.rename(columns= {'Name': 'Title', 'pagesNumber': 'Pages', 'CountsOfReview': 'ReviewCounts', 'PagesNumber': 'Pages', 'Authors': 'Author'}, inplace= True)
-        data = data[['Title', 'Author', 'Description', 'Pages', 'PublishYear', 'Language', 'Rating', 
-                     'Genres', 'ReviewCounts', 'isBestSeller', 'isEditorsPick', 'isGoodReadsChoice']]
+        for file in os.listdir(path_to_files):
+            print(file)
+            data = pd.read_csv(path_to_files + "/" + file)
+            data.rename(columns= {'Name': 'Title', 'pagesNumber': 'Pages', 'CountsOfReview': 'ReviewCounts', 'PagesNumber': 'Pages', 'Authors': 'Author'}, inplace= True)
+            data = data[['Title', 'Author', 'Description', 'Pages', 'PublishYear', 'Language', 'Rating', 
+                        'Genres', 'ReviewCounts', 'isBestSeller', 'isEditorsPick', 'isGoodReadsChoice']]
 
-        for index, row in data.iterrows():
-            print(self._make_msg(row))
-            print("------------------------------------------------------------------------------------")
+            for index, row in data.iterrows():
+                if pd.isna(row["Title"]):
+                    continue
+                
+                msg = self._make_msg(row)
+                self.kafka_connector.send(msg=msg)
+                print(msg)
+                print("------------------------------------------------------------------------------------")
     
     def _make_msg(self, row) -> dict:
         return {
+            "id": self._make_document_id(row["Title"]),
             "title": self._check_str(row["Title"]),
             "author": self._handle_str_to_list(row["Author"]),
             "description": self._convert_html_to_text(row["Description"]),
-            "pages": self._str_to_int(row["Pages"]),
-            "publish_year": self._str_to_int(row["PublishYear"]),
+            "pages": self._get_number(row["Pages"]),
+            "publish_year": self._str_to_year(row["PublishYear"]),
             "language": self._check_str(row["Language"]),
             "rating": self._str_to_float(row["Rating"]),
             "genres": self._handle_str_to_list(row["Genres"]),
@@ -34,6 +47,22 @@ class BookCsvReader:
             "is_editors_pick": self._str_to_bool(row["isEditorsPick"]),
             "is_goodreads_choice": self._str_to_bool(row["isGoodReadsChoice"]),
         }
+    
+    def _make_document_id(self, name: str) -> str:
+        stripped_name = name.strip()
+        result = ''
+
+        for char in stripped_name:
+            if 'A' <= char <= 'Z':
+                result += chr(ord(char) + 32)
+            elif char == ' ':
+                if len(result) > 0 and result[len(result) - 1] != '-':
+                    result += '-'
+            else:
+                if char not in self.special_chars_set:
+                    result += char
+
+        return result.strip()
     
     def _convert_html_to_text(self, html) -> str | None:
         if pd.isna(html):
@@ -49,28 +78,62 @@ class BookCsvReader:
             author_list = ast.literal_eval(string)
             return author_list
         else:
-            return [string]
+            return string.split(',')
     
     def _str_to_int(self, string) -> int | None:
         if pd.isna(string):
             return None
         
-        return int(string)
+        result = int(string)
+        if result == 0:
+            return None
+
+        return result
         
     def _str_to_float(self, string) -> float | None:
         if pd.isna(string):
             return None
         
-        return float(string)
+        result = float(string)
+        if result == 0:
+            return None
+        
+        return result
         
     def _str_to_bool(self, string) -> bool | None:
+        if isinstance(string, bool):
+            return string
+        
         if pd.isna(string):
             return None
         
         return True if (str.lower(string.strip()) == "true") else False
+        
+    def _str_to_year(self, string) -> int | None:
+
+        if isinstance(string, int):
+            return string
+
+        if pd.isna(string):
+            return None
+        
+        match = re.search(self.year_regex, string)
+        return int(match.group()) if match != None else None
         
     def _check_str(self, string) -> bool | None:
         if pd.isna(string):
             return None
         
         return string
+    
+    def _get_number(self, string) -> int | None:
+        if pd.isna(string):
+            return None
+        
+        match = re.search(self.number_regex, string)
+        result = int(match.group().replace(',', '')) if match != None else None
+
+        if result == 0:
+            return None
+        
+        return result
